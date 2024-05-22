e0e8ae045598beffed5cd15c8f3aabfe66a8e216
nimthara <nim20021127@gmail.com>
2024-05-22 11:08:26 +0530

    added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -15,8 +15,8 @@ class _homepageState extends State<homepage> {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-                  backgroundColor: Color.fromARGB(255, 20, 20, 20),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 20, 20, 20),
           leading: IconButton(
               onPressed: () {},
               icon: Icon(
@@ -40,8 +40,40 @@ class _homepageState extends State<homepage> {
                   color: const Color.fromARGB(255, 230, 254, 88),
                 ))
           ],
-      ),
-
+        ),
+        bottomNavigationBar: Container(
+          height: height / 11,
+          child: BottomNavigationBar(
+              iconSize: width / 12,
+              unselectedFontSize: width / 40,
+              selectedFontSize: width / 40,
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Color.fromARGB(255, 20, 20, 20),
+              selectedItemColor: const Color.fromARGB(255, 157, 178, 206),
+              unselectedItemColor: const Color.fromARGB(255, 157, 178, 206),
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.favorite_border,
+                  ),
+                  label: "Favourite",
+                ),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.search), label: "Search"),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home_outlined,
+                    color: const Color.fromARGB(255, 230, 254, 88),
+                  ),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.shopping_cart_outlined), label: "Cart"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.person), label: "Profile"),
+              ]),
+        ),
+        backgroundColor: Color.fromARGB(255, 20, 20, 20),
         body: Stack(
           children: [
             Container(
@@ -85,12 +117,14 @@ class _homepageState extends State<homepage> {
                                 image: AssetImage("assetss/Rectangle 50.png"),
                                 fit: BoxFit.contain)),
                         child: Padding(
-                          padding: EdgeInsets.only(top: 10, left: 13, right: 13),
+                          padding:
+                              EdgeInsets.only(top: 10, left: 13, right: 13),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
                                   Text(
                                     "Peugeot",
@@ -103,9 +137,10 @@ class _homepageState extends State<homepage> {
                                     height: 28,
                                     width: 47,
                                     decoration: BoxDecoration(
-                                        color:
-                                            const Color.fromARGB(255, 19, 19, 19),
-                                        borderRadius: BorderRadius.circular(20)),
+                                        color: const Color.fromARGB(
+                                            255, 19, 19, 19),
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
                                     child: Center(
                                       child: Image.asset(
                                           "assetss/material-symbols_star.png"),
@@ -120,30 +155,38 @@ class _homepageState extends State<homepage> {
                                     fontWeight: FontWeight.bold,
                                     fontSize: 24),
                               ),
-                              SizedBox(height: height/5.2,),
-                              Center(child: Text("500 Dh / Day",style: TextStyle(color: const Color.fromARGB(255, 230, 254, 88),fontSize: 20),))
+                              SizedBox(
+                                height: height / 5.2,
+                              ),
+                              Center(
+                                  child: Text(
+                                "500 Dh / Day",
+                                style: TextStyle(
+                                    color:
+                                        const Color.fromARGB(255, 230, 254, 88),
+                                    fontSize: 20),
+                              ))
                             ],
                           ),
                         ),
                       ),
-
                     ],
                   ),
                 ),
               ),
             ),
-                                  Positioned(
-                        top: 117,
-                        left: 10,
-                        child: Container(
-                            height: 170,
-                            width: 244,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage("assetss/car1.png"),
-                                    fit: BoxFit.contain)),
-                          ),
-                      )
+            Positioned(
+              top: 117,
+              left: 10,
+              child: Container(
+                height: 170,
+                width: 244,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assetss/car1.png"),
+                        fit: BoxFit.contain)),
+              ),
+            )
           ],
         ));
   }
