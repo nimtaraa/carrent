c334391e595652a29eb5f4a9fb101f8c1b9af084
nimthara <nim20021127@gmail.com>
2024-05-22 21:10:13 +0530

    firebaseadded

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: spashscreen(),
     );
   }
