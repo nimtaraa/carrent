3fa851c5c6e2a95a240b7cb9d1cbc9dc8aa0bbba
nimthara <nim20021127@gmail.com>
2024-05-22 21:16:46 +0530

    add things to home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
-import 'package:carrent/viewpage.dart';
+import 'package:carrent/viewcontainer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -15,6 +16,7 @@ class _homepageState extends State<homepage> {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+    final vehicals=FirebaseFirestore.instance.collection("vehicals").snapshots();
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromARGB(255, 20, 20, 20),
@@ -110,91 +112,58 @@ class _homepageState extends State<homepage> {
                           )
                         ],
                       ),
+                      SizedBox(height: height/30,),
                       Container(
-                        height: 270,
-                        width: 208,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage("assetss/Rectangle 50.png"),
-                                fit: BoxFit.contain)),
-                        child: Padding(
-                          padding:
-                              EdgeInsets.only(top: 10, left: 13, right: 13),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "Peugeot",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 24),
-                                  ),
-                                  Container(
-                                    height: 28,
-                                    width: 47,
-                                    decoration: BoxDecoration(
-                                        color: const Color.fromARGB(
-                                            255, 19, 19, 19),
-                                        borderRadius:
-                                            BorderRadius.circular(20)),
-                                    child: Center(
-                                      child: Image.asset(
-                                          "assetss/material-symbols_star.png"),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              Text(
-                                "E208",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 24),
-                              ),
-                              SizedBox(
-                                height: height / 5.2,
-                              ),
-                              Center(
-                                  child: Text(
-                                "500 Dh / Day",
-                                style: TextStyle(
-                                    color:
-                                        const Color.fromARGB(255, 230, 254, 88),
-                                    fontSize: 20),
-                              ))
-                            ],
-                          ),
-                        ),
+                        height: 300,
+                       width: double.infinity,
+                       child: StreamBuilder<QuerySnapshot>(
+                        stream: vehicals,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
+                            return Text("Connection Error");
+                            
+                          }
+                          if (snapshot.connectionState==ConnectionState.waiting) {
+                            return Text("Loading..");
+                          }
+                          var vehicaldoc=snapshot.data!.docs;
+
+                          return ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: vehicaldoc.length,
+                            itemBuilder:(context, index) {
+                              return Padding(
+                                padding:  EdgeInsets.only(right: width/20),
+                                child: viewcontainer(
+                                  uname: vehicaldoc[index]['uname'], 
+                                  lname: vehicaldoc[index]['lname'], 
+                                  image: vehicaldoc[index]['image'], 
+                                  dh:vehicaldoc[index]['dh value'], 
+                                  battery: vehicaldoc[index]['battery'], 
+                                  overview: vehicaldoc[index]['overview'], 
+                                  speed:vehicaldoc[index]['speed'],
+                                   seat: vehicaldoc[index]['seat'],
+                                
+                                
+                                  
+                                ),
+                              );
+
+
+
+                            },
+                            
+                            );
+
+                        },
+                       ),
                       ),
+
                     ],
                   ),
                 ),
               ),
             ),
-            Positioned(
-              top: 117,
-              left: 10,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder:(context) {
-                    return viewpage();
-                  },));
-                },
-                child: Container(
-                  height: 170,
-                  width: 244,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("assetss/car1.png"),
-                          fit: BoxFit.contain)),
-                ),
-              ),
-            )
           ],
         ));
   }
